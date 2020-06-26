9b7b63aa4ed258c387823e80e262f8aee0bcd236
eyadal <31968847+eyadal@users.noreply.github.com>
2020-06-26 23:51:18 +0200

    Arrays of objects

--- main.js
+++ main.js
@@ -74,4 +74,25 @@ person.email = 'test123@gmail.com'
 // console.log(person.hobbies[1])
 // console.log(person.address.city)
 const  {firstName, lastName, address: {city}} = person
-console.log(person.email)
+// console.log(person)
+
+//========================================================================//
+// Array of objects
+const todos = [
+    {
+        id: 1,
+        text: 'Take out trash',
+        isCompleted: true
+    },
+    {
+        id: 2,
+        text: 'Meeting with boss',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        text: 'Dentist appt',
+        isCompleted: false
+    }
+]
+console.log(todos[1].text)
